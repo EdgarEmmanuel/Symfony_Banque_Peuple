9f320e7b566082d06036c26b3f640e7b1256214d
Edgar <mmnledgar@gmail.com>
2020-11-18 09:23:49 +0000

    function_handling_insert_respo

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use App\Entity\ResponsableCompte;
 
 class AdminController extends AbstractController
 {
     private $ent;
     private $session_admin;
+    private $respoCompte ;
 
     public function __construct(EntityManagerInterface $entity){
         $this->ent = $entity;
         $this->session_admin = new Session();
+        $this->respoCompte = $this->ent->getRepository(ResponsableCompte::class);
     }
 
     public function indexPage(){
@@ -23,6 +27,11 @@ class AdminController extends AbstractController
     public function deconnexionAdmin(){
         $this->session_admin->set("nom_Complet_admin",null);
         return $this->redirectToRoute("index");
+    }
+
+    public function insertResponsable(Request $request){
+        var_dump($request->request->get("nom_respo"));
+        die();
     }
 
 
